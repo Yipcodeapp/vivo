6039a1e9855d9c98cc1bfeb7f3cdb430285ca64e
Yipcodeapp <yourappiptv@gmail.com>
2025-08-06 22:05:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 
 def get_valid_stream_url():
     """Obtiene la URL del stream ignorando CDN variable"""
-    url = "https://envivofy.com/canal.php?stream=winplus"
+    url = "https://librefutboltv.su/tyc-sports/"
     
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
